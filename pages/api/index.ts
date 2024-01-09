0ba0ecf07f59ec4c5bbb7b193220d4e3f77f340c
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2024-01-09 00:32:21 -0300

    cambios en git ignore

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,4 +1,4 @@
 import type{NextApiRequest,NextApiResponse} from "next"
 export default function (req:NextApiRequest,res:NextApiResponse){
-res.send("hola soy la api")
+res.send("hola soy la api"+ process.env.FIREBASE_CONNECTION)
 }
